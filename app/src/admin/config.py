b3c7a8d18cbc9964933018da23725dd9c9f17be7
levchuk <swankyyy1@gmail.com>
2025-04-05 18:42:33 +0300

    add client ro admin

--- app/src/admin/config.py
+++ app/src/admin/config.py
@@ -11,6 +11,7 @@ from .models import (
     ProfessionAdmin,
     SectionAdmin,
     SectionImageAdmin,
+    ClientAdmin,
 )
 
 # List of views for the Admin Panel, add it from .models
@@ -21,19 +22,20 @@ list_views = [
     ProfessionAdmin,
     SectionImageAdmin,
     SectionAdmin,
+    ClientAdmin,
 ]
 
 
 def create_admin(app):
     """Connect Admin Panel to database and to main app"""
-    
+
     # Initialize the Admin Panel with the database engine and authentication backend
     admin = Admin(
         app=app,
         engine=db_config.get_engine(),
         authentication_backend=AdminAuthBackend(secret_key=settings.SECRET_KEY),
     )
-    
+
     # Add views to the Admin Panel
     for view in list_views:
         admin.add_view(view)
